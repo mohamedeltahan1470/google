10aba032bacb7fe6f9a5700b29b6794dd45383f0
mohamedeltahan1470 <mohamedeltahan1470@gmail.com>
2024-05-29 06:25:20 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="div1">
+    This application is not yet complete<hr/>due to some problems<hr/>
+    <span>😥</span>
+    </div>;
+};
+
+export default Footer;
---
+++ src/components/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style.css";
+const Routes = () => {
+  return (
+    <div>
+        <h1 className='h1'>Nothing here </h1>
+    </div>
+  )
+}
+
+export default Routes
