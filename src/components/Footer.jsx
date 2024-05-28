dcdbdcc246c40b1313edb2b4b62fc62509b9bbba
mohamedeltahan1470 <mohamedeltahan1470@gmail.com>
2024-05-29 06:26:43 +0700

    changing1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return <div className="div1">
-    This application is not yet complete<hr/>due to some problems<hr/>
+    This application is not yet complete<hr/>due to some problems.<hr/>
     <span>😥</span>
     </div>;
 };
